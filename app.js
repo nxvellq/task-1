41dc5d1d2cb7e7eb9eb852c4e0b97b75cebda2df
nxvellq <nxvellq@gmail.com>
2025-01-09 18:15:57 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const http = require('http');
+const url = require('url');
 
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html; charset=utf-8'); 
-    res.end('<h1>Привет, Октагон!</h1>'); 
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+
+  res.setHeader('Content-Type', 'application/json');
+
+  if (path === '/static') {
+      const response = { header: "Hello", body: "Octagon NodeJS Test" };
+        res.statusCode = 200;
+        res.end(JSON.stringify(response));
+  } else if (path === '/dynamic') {
+        const { a, b, c } = parsedUrl.query;
+        if(a && b && c && !isNaN(a) && !isNaN(b) && !isNaN(c)) {
+            const result = (Number(a) + Number(b) + Number(c)) / 3;
+             const response = {header: "Calculated", body: String(result)};
+             res.statusCode = 200;
+              res.end(JSON.stringify(response));
+        } else {
+            const response = {header: "Error"};
+            res.statusCode = 400;
+             res.end(JSON.stringify(response));
+        }
+
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({header: "Not Found"}));
+  }
+
+
 });
 
-server.listen(3000, () => {
-    console.log('Сервер запущен на http://localhost:3000/');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
